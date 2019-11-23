f5b8b77cebab1255c1d105610a503bf9fad3df2f
root <sahare92@gmail.com>
2019-11-23 16:19:28 +0000

    base

---
+++ app/javascript/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <app-nav></app-nav>
+    <app-view>
+      <app-sidebar></app-sidebar>
+      <app-content></app-content>
+    </app-view>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue223322!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
